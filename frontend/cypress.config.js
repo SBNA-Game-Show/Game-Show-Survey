fd2085976b4ade8cbe312bfd07ab5336ebc9c0cc
Jacob Makil <makiljacob@gmail.com>
2025-12-02 09:39:53 -0500

    Changes to Cypress testing

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,18 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+
+    env: {
+      DB_NAME: "Data_Jimmy_Test",
+    },
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Force backend DB name for all Cypress runs
+      process.env.DB_NAME = "Data_Jimmy_Test";
+
+      return config;
     },
   },
 });
